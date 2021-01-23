172d15647a3d69009e87184543ee3e60b3616bde
krafasacv <43054099+krafasacv@users.noreply.github.com>
2021-01-22 20:25:22 -0600

    Add files via upload

---
+++ pos_agro/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Modificaciones POS AGROMORALES',
+    'version': '1.0',
+    'description': ''' Modificaciones al Punto de Venta para AGROMORALES.
+    ''',
+    'category': 'Point Of Sale,',
+    'author': 'KRAFA SA DE CV',
+    'website': 'www.krafa.com.mx',
+    'depends': [
+        'point_of_sale'
+    ],
+    'qweb': [
+        'static/src/xml/pos_agromorales.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'price': 0.00,
+    'currency': 'USD',
+    'license': 'OPL-1',	
+}
